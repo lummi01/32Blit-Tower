dceaf239dfa14c546206bd887f6e245068e114a6
Lummi <72337143+lummi01@users.noreply.github.com>
2024-05-13 18:53:16 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# 32Blit Tower
-Build your tower to the top of the screen.
-Get points for level, reaching the top and exact positioning.
-
-Building with A-Button.
-
-For 32Blit builds this is:
-```
-mkdir build.32stm
-cd build.32stm
-cmake .. -DCMAKE_TOOLCHAIN_FILE=/path/to/32blit-sdk/32blit.toolchain
-```
-
+# 32Blit Boilerplate
 
 For local builds this is:
 ```
--- tower.cpp
+++ tower.cpp
@@ -8,6 +8,7 @@ using namespace blit;
 
 Font font(font3x5);
 
+int state = 1;
 int xx;
 int step;
 int score;
@@ -53,47 +54,56 @@ void render(uint32_t time)
 	}
 
 	screen.pen = Pen(225,225,225);
-    screen.text("SCORE " + std::to_string(score), font, Point(1, 1));
+    screen.text("Score " + std::to_string(score), font, Point(1, 1));
+	if (state == 2)
+		screen.text("Game Over", font, Point(61, 57));
 }
 
 // update
 void update(uint32_t time) 
 {
-	block[step].x += xx;
-	if (block[step].x <= 0 || block[step].x + block[step - 1].width >= 159)
-		xx = -xx;
+	if (state == 1)
+	{
+		block[step].x += xx;
+		if (block[step].x <= 0 || block[step].x + block[step - 1].width >= 159)
+			xx = -xx;
 
-    if (buttons.pressed & Button::A)
-    {
-		block[step].width -= abs(block[step - 1].x - block[step].x);
-
-		if (block[step].width == block[step - 1].width)
+		if (buttons.pressed & Button::A)
 		{
-			score += 5;
-		}
-		else if (block[step].width > 0)
-		{
-			score += 1;
+			block[step].width -= abs(block[step - 1].x - block[step].x);
 
-			if (block[step].x < block[step - 1].x)
+			if (block[step].width == block[step - 1].width)
+			{
+				score += 5;
+			}
+			else if (block[step].width > 0)
+			{
+				score += 1;
+				if (block[step].x < block[step - 1].x)
+					block[step].x = block[step - 1].x;
+			}
+			else
+			{
+				state = 2;
+			}
+
+			step++;
+			if (step > 19)
+			{
+				score += block[step - 1].width;
+				start();
+			}
+			else
+			{
 				block[step].x = block[step - 1].x;
+				block[step].width = block[step - 1].width;
+			}
 		}
-		else
-		{
+	}
+	else if (state == 2 && buttons.pressed & Button::A)
+	{
 			score = 0;
 			start();
-		}
-
-		step++;
-		if (step > 19)
-		{
-			score += block[step - 1].width;
-			start();
-		}
-		else
-		{
-			block[step].x = block[step - 1].x;
-			block[step].width = block[step - 1].width;
-		}
-    }
+			state = 1;
+	}
 }
