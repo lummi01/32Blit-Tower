aa89ba9e496c62c48677c72e6b38e429bdfef226
Lummi <72337143+lummi01@users.noreply.github.com>
2024-05-13 15:57:43 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# 32Blit Boilerplate
+
+For local builds this is:
+```
+mkdir build
+cd build
+cmake -D32BLIT_DIR=/path/to/32blit-sdk/ ..
+```
---
+++ tower.cpp
@@ -0,0 +1,99 @@
+// Tower
+// 2024 M. Gerloff
+
+#include "tower.hpp"
+#include "assets.hpp"
+
+using namespace blit;
+
+Font font(font3x5);
+
+int xx;
+int step;
+int score;
+
+struct Block
+{
+	int x;
+	int width;
+};
+
+Block block[20];
+
+void start()
+{
+	block[0].x = 40;
+	block[0].width = 80;
+	block[1].x = 0;
+	block[1].width = 80;
+	xx = 1;
+	step = 1;
+}
+
+// init
+void init() 
+{
+    set_screen_mode(ScreenMode::lores);
+	start();
+}
+
+// render
+void render(uint32_t time) 
+{
+    screen.pen = Pen(0, 0, 0);
+    screen.clear();
+
+    screen.alpha = 255;
+    screen.mask = nullptr;
+
+	for (int i = 0; i <= step; i++)
+	{
+	    screen.pen = Pen(10 + (i * 8), 150 + (i * 5), 10 + (i * 8));
+	    screen.rectangle(Rect(block[i].x, 115 - (i * 6), block[i].width, 5));
+	}
+
+	screen.pen = Pen(225,225,225);
+    screen.text("SCORE " + std::to_string(score), font, Point(1, 1));
+}
+
+// update
+void update(uint32_t time) 
+{
+	block[step].x += xx;
+	if (block[step].x <= 0 || block[step].x + block[step - 1].width >= 159)
+		xx = -xx;
+
+    if (buttons.pressed & Button::A)
+    {
+		block[step].width -= abs(block[step - 1].x - block[step].x);
+
+		if (block[step].width == block[step - 1].width)
+		{
+			score += 5;
+		}
+		else if (block[step].width > 0)
+		{
+			score += 1;
+
+			if (block[step].x < block[step - 1].x)
+				block[step].x = block[step - 1].x;
+		}
+		else
+		{
+			score = 0;
+			start();
+		}
+
+		step++;
+		if (step > 19)
+		{
+			score += block[step - 1].width;
+			start();
+		}
+		else
+		{
+			block[step].x = block[step - 1].x;
+			block[step].width = block[step - 1].width;
+		}
+    }
+}
