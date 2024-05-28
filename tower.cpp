de051069fce01de8fd5c8eb87cd62e33c6485548
Lummi <72337143+lummi01@users.noreply.github.com>
2024-05-28 14:36:22 +0200

    Add files via upload

--- tower.cpp
+++ tower.cpp
@@ -6,27 +6,37 @@
 
 using namespace blit;
 
-Font font(font3x5);
-
 int state = 1;
 int xx;
+int width = 80;
 int step;
 int score;
 
-struct Block
+struct Fade
 {
-	int x;
-	int width;
+    int alpha;
+    int c1;
+    int c2;
+    int x;
+    int y;
+    int width;
 };
 
-Block block[20];
+struct Block
+{
+    int x1;
+    int x2;
+};
+
+Block block[21];
+Fade fade;
 
 void start()
 {
-	block[0].x = 40;
-	block[0].width = 80;
-	block[1].x = 0;
-	block[1].width = 80;
+	block[0].x1 = 80 - (width / 2);
+	block[0].x2 = block[0].x1 + width;
+	block[1].x1 = 0;
+	block[1].x2 = width;
 	xx = 1;
 	step = 1;
 }
@@ -49,14 +59,25 @@ void render(uint32_t time)
 
 	for (int i = 0; i <= step; i++)
 	{
-	    screen.pen = Pen(10 + (i * 8), 150 + (i * 5), 10 + (i * 8));
-	    screen.rectangle(Rect(block[i].x, 115 - (i * 6), block[i].width, 5));
+	    screen.pen = Pen((i * 10), 150 + (i * 5), (i * 10));
+	    screen.rectangle(Rect(block[i].x1, 115 - (i * 6), block[i].x2 - block[i].x1, 5));
 	}
 
-	screen.pen = Pen(225,225,225);
-    screen.text("Score " + std::to_string(score), font, Point(1, 1));
+    if (fade.alpha > 0)
+    {
+        screen.alpha = fade.alpha;
+        screen.pen = Pen(fade.c1,fade.c2,fade.c1);
+	    screen.rectangle(Rect(fade.x, fade.y, fade.width, 5));
+        screen.alpha = 255;
+    }
+
+	screen.pen = Pen(150,150,150);
+    screen.text("score", minimal_font, Point(1, 0));
+
+	screen.pen = Pen(250,250,250);
+    screen.text(std::to_string(score), minimal_font, Point(30, 0));
 	if (state == 2)
-		screen.text("Game Over", font, Point(61, 57));
+		screen.text("game over", minimal_font, Point(58, 57));
 }
 
 // update
@@ -64,45 +85,81 @@ void update(uint32_t time)
 {
 	if (state == 1)
 	{
-		block[step].x += xx;
-		if (block[step].x <= 0 || block[step].x + block[step - 1].width >= 159)
-			xx = -xx;
+        fade.alpha -= 5;
 
-		if (buttons.pressed & Button::A)
-		{
-			block[step].width -= abs(block[step - 1].x - block[step].x);
+        if (step < 20)
+        {
+		    block[step].x1 += xx;
+		    block[step].x2 += xx;
 
-			if (block[step].width == block[step - 1].width)
-			{
-				score += 5;
-			}
-			else if (block[step].width > 0)
-			{
-				score += 1;
-				if (block[step].x < block[step - 1].x)
-					block[step].x = block[step - 1].x;
-			}
-			else
-			{
-				state = 2;
-			}
+		    if (block[step].x1 <= 0 || block[step].x2 >= 159)
+			    xx = -xx;
 
-			step++;
-			if (step > 19)
-			{
-				score += block[step - 1].width;
-				start();
-			}
-			else
-			{
-				block[step].x = block[step - 1].x;
-				block[step].width = block[step - 1].width;
-			}
-		}
+		    if (buttons.pressed & Button::A)
+		    {
+			    if (block[step].x2 <= block[step - 1].x1 || block[step].x1 >= block[step - 1].x2)
+			    {
+                    step--;
+				    state = 2;
+			    }
+                else 
+                {
+                    if (block[step].x1 > block[step - 1].x1)
+			        {
+				        score += 1;
+
+                        fade.alpha = 255;
+                        fade.c1 = step * 10;
+                        fade.c2 = step * 5 + 150;
+                        fade.x = block[step - 1].x2;
+                        fade.y = 115 - (step * 6);
+                        fade.width = block[step].x1 - block[step - 1].x1;
+
+				        block[step].x2 = block[step - 1].x2;
+			        }
+                    else if (block[step].x1 < block[step - 1].x1)
+			        {
+				        score += 1;
+
+                        fade.alpha = 255;
+                        fade.c1 = step * 10;
+                        fade.c2 = step * 5 + 150;
+                        fade.x = block[step].x1;
+                        fade.y = 115 - (step * 6);
+                        fade.width = block[step - 1].x1 - block[step].x1;
+
+				        block[step].x1 = block[step - 1].x1;
+			        }
+                    else
+			        {
+				        score += 5;
+
+                        fade.alpha = 255;
+                        fade.c1 = 255;
+                        fade.c2 = 255;
+                        fade.x = block[step].x1;
+                        fade.y = 115 - (step * 6);
+                        fade.width = block[step].x2 - block[step].x1;
+			        }
+
+
+			        step++;
+			        block[step].x1 = block[step - 1].x1;
+			        block[step].x2 = block[step - 1].x2;
+                }
+		    }
+        }
+        else if (fade.alpha < 0)
+        {
+		    score += (block[step].x2 - block[step].x1);
+            width -= 8;
+		    start();
+	    }
 	}
 	else if (state == 2 && buttons.pressed & Button::A)
 	{
 			score = 0;
+            width = 80;
 			start();
 			state = 1;
 	}
